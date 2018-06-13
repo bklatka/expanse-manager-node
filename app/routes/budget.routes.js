c7f03b1dc57e05ede2ef5a944af580768bca5cc2
Bernard Klatka <bernard@Bernards-MacBook-Pro.local>
2018-06-13 20:21:26 +0200

    Adding POST expense to MongoDB

--- app/routes/budget.routes.js
+++ app/routes/budget.routes.js
@@ -1,6 +1,16 @@
 module.exports = (app, db) => {
   app.post('/add', (req, res) => {
-    console.log(req.body);
-    res.send('Hello world');
+    const expense = {
+      name: req.body.name,
+      type: req.body.type,
+      value: req.body.value
+    };
+    db.collection('expenses').insert(expense, (err, result) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MongoDBClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 const APP_PORT = 3000;
@@ -8,7 +9,14 @@ const APP_PORT = 3000;
 const routes = require('./app/routes/index');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-routes(app, {});
-app.listen(APP_PORT, () => {
-  console.log('Server running on port ' + APP_PORT);
-});
+
+MongoDBClient.connect(
+  db.url,
+  (err, database) => {
+    if (err) return console.log(err);
+    routes(app, database);
+    app.listen(APP_PORT, () => {
+      console.log('We are live on ' + APP_PORT);
+    });
+  }
+);
