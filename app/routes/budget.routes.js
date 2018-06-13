f53bbd6bb985695f4a1117b86de7a3a7544551a6
Bernard Klatka <bernard@Bernards-MacBook-Pro.local>
2018-06-13 20:54:19 +0200

    CRUD operations

--- app/routes/budget.routes.js
+++ app/routes/budget.routes.js
@@ -1,5 +1,7 @@
+const ObjectId = require('mongodb').ObjectId;
+
 module.exports = (app, db) => {
-  app.post('/add', (req, res) => {
+  app.post('/expenses', (req, res) => {
     const expense = {
       name: req.body.name,
       type: req.body.type,
@@ -10,6 +12,55 @@ module.exports = (app, db) => {
         res.send({ error: 'An error has occurred' });
       } else {
         res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete('/expenses/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectId(id) };
+    db.collection('expenses').remove(details, (err, item) => {
+      if (err) {
+        res.send({ error: 'Error removing object' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.put('/expenses/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectId(id) };
+    const updated = { name: res.body.name, value: res.body.value };
+    db.collection('expenses').update(details, updated, (err, item) => {
+      if (err) {
+        res.send({ error: 'Error occured' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.get('/expenses', (req, res) => {
+    db.collection('expenses')
+      .find({})
+      .toArray((err, list) => {
+        if (err) {
+          res.send({ error: 'Error getting data' });
+        } else {
+          res.send(list);
+        }
+      });
+  });
+
+  app.get('/expenses/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectId(id) };
+    db.collection('expenses').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        res.send(item);
       }
     });
   });
--- server.js
+++ server.js
@@ -14,7 +14,10 @@ MongoDBClient.connect(
   db.url,
   (err, database) => {
     if (err) return console.log(err);
+    console.log('Connected to DB');
+
     routes(app, database);
+
     app.listen(APP_PORT, () => {
       console.log('We are live on ' + APP_PORT);
     });
